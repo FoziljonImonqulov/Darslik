be0a98b2286ef525c258c4ae4e03f44c5d93b070
FoziljonImonqulov <imonqulovf1234@gmail.com>
2023-08-15 17:08:28 +0500

    New codes are added

--- Telegram_bot/image_to_pdf/main.py
+++ Telegram_bot/image_to_pdf/main.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
@@ -6,40 +8,67 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+from Telegram_bot.image_to_pdf.sql1 import Tbot
+
 BOT_TOKEN = '6593693542:AAHZNVVPju5nesUbB6HnL0RJoflI-HKx5qM'
 
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
+logger = logging.getLogger(__name__)
 
 
 class Register(StatesGroup):
     fullname = State()
     phone_number = State()
     language = State()
+    first_name = State()
+    city = State()
+    lang = State()
+
+
+class inside_o(StatesGroup):
+    first_name = State()
+    city = State()
+    lang = State()
 
 
 @dp.message_handler(commands="start")
 async def register_handler(message: types.Message):
-    await Register.fullname.set()
-    await message.answer("To'liq ism familya : ")
-
-
-@dp.message_handler(state=Register.fullname)
-async def fullname_handler(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data["fullname"] = message.text
-    await Register.phone_number.set()
+    a = message['from']['id']
+    data = {
+        "user_id": message["from"]['id'],
+        'is_bot': message['from']['is_bot'],
+        "first_name": message['from']['first_name'],
+        'last_name': message['from']['last_name'],
+        'username': message['from']['username'],
+        'language_code': message['from']['language_code']
+    }
     markup = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-    markup.add(KeyboardButton("☎️phone number", request_contact=True))
-    await message.answer("Telefon raqamingizni yuboring : ", reply_markup=markup)
+    markup.add(KeyboardButton("Telefon raqamingizni yuboring", request_contact=True))
+    obj = Tbot(**data)
+    if not obj.check_is_available():
+        obj.insert_data()
+    await message.answer('Iltimos royxatdan o\'ting', reply_markup=markup)
 
 
-@dp.message_handler(content_types=types.ContentTypes.CONTACT, state=Register.phone_number)
+@dp.message_handler(content_types=types.ContentTypes.CONTACT)
+async def fullname_handler(message: types.Message, state: FSMContext):
+    contact = message.contact.phone_number
+    await Register.first_name.set()
+    async with state.proxy() as data:
+        data["phone_number"] = contact
+    await message.answer("Ismingizni yozib yuboring: ")
+
+
+@dp.message_handler(state=Register.first_name)
 async def contact_handler(message: types.Message, state: FSMContext):
     async with state.proxy() as data:
-        data["phone_number"] = message.contact.phone_number
+        data["first_name"] = message.text
     data = await state.get_data()
-    await message.answer(f"{data.get('fullname')} {data.get('phone_number')}")
+    await message.answer(f"{data.get('first_name')} {data.get('phone_number')}")
+    obj = Tbot(**data)
+    if obj:
+        obj.update_data()
     design = [
         [InlineKeyboardButton("Ha", callback_data='ha'), InlineKeyboardButton("yo'q", callback_data='yoq')]
     ]
@@ -58,20 +87,29 @@ async def main_menu(callback: types.CallbackQuery):
         markup.add(KeyboardButton("⚙️ Sozlamalar"))
         await callback.message.answer("Main menu ", reply_markup=markup)
     else:
-        await register_handler()
+        await register_handler(callback.message)
 
 
 @dp.message_handler(lambda msg: msg.text in ("🍽 Menu"))
 async def menu_(message: types.Message):
     markup = ReplyKeyboardMarkup(resize_keyboard=True)
     markup.add(KeyboardButton(" 🚙 yetkazib berish "), KeyboardButton(" 🚲 Olib ketish"))
-    markup.add(KeyboardButton("Ortga ⬅"))
+    markup.add(KeyboardButton("⬅Ortga"))
     await message.answer("O'zingizga qulay usluni tanlang: ", reply_markup=markup)
 
 
-@dp.message_handler(lambda msg: msg.text in ("Ortga ⬅"))
+@dp.message_handler(lambda msg: msg.text.startswith("⬅Ortga"))
 async def back(message: types.Message):
-    await main_menu()
+    await main_menu(message)
+
+
+async def main_menu(reply_message: types.Message):
+    markup = ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.add(KeyboardButton("🍽 Menu"))
+    markup.add(KeyboardButton("🛒 Buyrtmalar tarixi"), KeyboardButton("✍  Fikr bildirish"))
+    markup.add(KeyboardButton(" 🗒 Ma'lumot"), KeyboardButton("📞 Biz bilan aloqa"))
+    markup.add(KeyboardButton("⚙️ Sozlamalar"))
+    await reply_message.answer("Main menu ", reply_markup=markup)
 
 
 @dp.message_handler(lambda msg: msg.text in (" 🚲 Olib ketish"))
@@ -79,7 +117,7 @@ async def by_me(message: types.Message):
     await message.answer("Tez orada...")
 
 
-@dp.message_handler(lambda msg: msg.text in (" 🚙 yetkazib berish "))
+@dp.message_handler(lambda msg: msg.text in ("🚙 yetkazib berish "))
 async def by_me(message: types.Message):
     await message.answer("Tez orada...")
 
@@ -97,18 +135,39 @@ async def orders_history(message: types.Message):
     markup.add("Yoqmadi ⭐ ⭐ ⭐")
     markup.add("Yomon ⭐ ⭐ ")
     markup.add("Juda yomon 👎")
-    markup.add("⬅ortga")
-    await message.answer("Siz hali buyurtma bermadingiz ", reply_markup=markup)
+    markup.add("⬅Ortga")
+    await message.answer("Fikringizni belgilang:", reply_markup=markup)
 
 
-@dp.message_handler(lambda msg: msg.text in ("⬅ortga"))
+@dp.message_handler(
+    lambda msg: msg in ("Hammasi yoqdi", "Yaxshi ⭐ ⭐ ⭐ ⭐", "Yoqmadi ⭐ ⭐ ⭐", "Yomon ⭐ ⭐ ", "Juda yomon 👎"))
+async def get_sms(message: types.Message):
+    chat_id = 1526992254
+    if message.text == "Hammasi yoqdi":
+        response = "Hammasi yoqdi"
+        await message.bot.send_message(chat_id, response)
+    elif message.text == "Yaxshi ⭐ ⭐ ⭐ ⭐":
+        response = "Yaxshi ⭐ ⭐ ⭐ ⭐"  # Corrected the variable name
+        await message.bot.send_message(chat_id, response)
+    elif message.text == "Yoqmadi ⭐ ⭐ ⭐":
+        response = "Yoqmadi ⭐ ⭐ ⭐"
+        await message.bot.send_message(chat_id, response)
+    elif message.text == "Yomon ⭐ ⭐ ":
+        response = "Yomon ⭐ ⭐ "
+        await message.bot.send_message(chat_id, response)
+    else:
+        response = "Juda yomon 👎"
+        await message.bot.send_message(chat_id, response)
+
+
+@dp.message_handler(lambda msg: msg.text in ("⬅Ortga"))
 async def back(message: types.Message):
-    await main_menu()
+    await main_menu(message)
 
 
 @dp.message_handler(lambda msg: msg.text in ("📞 Biz bilan aloqa"))
 async def aloqa(message: types.Message):
-    await message.bot.send_contact("Bizning admin", "+99893iin4113", "Imonqulov Foziljon")
+    await message.bot.send_contact(message.chat.id, "+99893iin4113", "Imonqulov", "Foziljon")
 
 
 @dp.message_handler(lambda msg: msg.text in ("🗒 Ma'lumot"))
@@ -125,9 +184,32 @@ async def settings(message: types.Message):
     await message.answer("Harakatni tanlang:", reply_markup=markup)
 
 
+@dp.message_handler(lambda msg: msg.text in ("Ismni o'zgartishish"))
+async def change_name(message: types.Message, state: FSMContext):
+    await inside_o.first_name.set()
+    await message.answer("Tez orada")
+
+
+# @dp.message_handler(state=inside_o.first_name)
+# async def changed_name(message: types.Message, state: FSMContext):
+#     async with state.proxy() as data:
+#         data['first_name'] = message.text
+#     a = {'user_id': message.from_user.id}
+#     data.update(a)
+#     await state.finish()
+#     obj = Tbot(**data)
+#     obj.update_name()
+#     await message.answer(f"Ismingiz {data['first_name']} ga o'zgarid ")
+
+
 @dp.message_handler(lambda msg: msg.text in ("⬅Ortga"))
 async def back(message: types.Message):
-    await main_menu()
+    await main_menu(message)
+
+
+@dp.message_handler(commands='salom')
+async def send_(message: types.Message):
+    await message.bot.send_message(1526992254, "Salom")
 
 
 if __name__ == '__main__':
--- Telegram_bot/image_to_pdf/sql1.py
+++ Telegram_bot/image_to_pdf/sql1.py
@@ -1,26 +1,68 @@
-# from aiogram import Bot, Dispatcher, types, executor
-# import img2pdf
-# import fpdf as FPDF
-# from PIL import Image
-# import os
-#
-# bot_token = "6593693542:AAHZNVVPju5nesUbB6HnL0RJoflI-HKx5qM"
-#
-# bot = Bot(bot_token)
-# dp = Dispatcher(bot)
-#
-#
-# @dp.message_handler(commands=['start'])
-# async def start_bot(message: types.Message):
-#     text = f"Assalomu alaykum {message.from_user.first_name} Image to Pdf botiga xush kelibsiz"
-#     await message.answer(text)
-#     await message.answer('Botdan foydalanish uchun rasmlarni yuboring')
-#
-#
-# @dp.message_handler(content_types=types.ContentType.PHOTO)
-# async def convert_to_pdf(message: types.Message):
-#
-#
-#
-# if __name__ == '__main__':
-#     executor.start_polling(dp, skip_updates=True)
+from dataclasses import dataclass
+
+import psycopg2
+
+con = psycopg2.connect(
+    database='postgres',
+    user='postgres',
+    host='localhost',
+    port=5432,
+    password='11'
+)
+cur = con.cursor()
+
+
+@dataclass
+class Tbot:
+    user_id: int = None
+    is_bot: str = None,
+    first_name: str = None,
+    last_name: str = None,
+    username: str = None,
+    language_code: str = None,
+    phone_number: str = None
+
+    def insert_data(self):
+        query = """
+        insert into users(user_id,is_bot,first_name,last_name,username,language_code)
+        values (%s,%s,%s,%s,%s,%s)
+         """
+        parametrs = (self.user_id, self.is_bot, self.first_name, self.last_name, self.username, self.language_code)
+
+        cur.execute(query, parametrs)
+        con.commit()
+
+    def check_is_available(self):
+        query = """
+            select * from users where user_id = %s
+        """
+        param = (self.user_id,)
+        cur.execute(query, param)
+        if cur.fetchall():
+            return True
+        return False
+
+    def update_data(self):
+        query = """
+            update users set phone_number=%s,first_name=%s where user_id = %s
+        """
+        params = (self.phone_number, self.first_name, self.user_id)
+        cur.execute(query, params)
+        con.commit()
+
+    def update_name(self):
+        query = """
+            update users set first_name=%s where user_id = %s
+        """
+        params = (self.first_name,self.user_id)
+        cur.execute(query, params)
+        con.commit()
+
+    def update_number(self):
+        query = """
+            update users set phone_number=%s where user_id = %s
+        """
+        params = (self.phone_number,self.user_id)
+        cur.execute(query, params)
+        con.commit()
+
