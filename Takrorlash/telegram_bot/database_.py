b9e0ff00bd1dbd00c91117314bd325a548f4b648
userfoziljon <imonqulovf1234@gmail.com>
2023-09-08 09:38:42 +0500

    new files

---
+++ Takrorlash/telegram_bot/database_.py
@@ -0,0 +1,42 @@
+from faker import Faker
+from sqlalchemy import create_engine, String, Column, Integer, Text, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+
+faker = Faker()
+engine = create_engine("postgresql+psycopg2://postgres:1@pg/postgres")
+Session = sessionmaker(engine)
+session = Session()
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'  # noqa
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer)
+    first_name = Column(String(30))
+    last_name = Column(String(30))
+    username = Column(String(30))
+    is_bot = Column(String(10))
+    language = Column(String(10))
+    long = Column(String(20))
+    lat = Column(String(20))
+
+    # comments = relationship("Comment", back_populates="post")
+
+    def __repr__(self):
+        return f"{self.__dict__}"
+
+
+class Exercise(Base):
+    __tablename__ = 'exercises'  # noqa
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    exercise = Column(String(255))
+
+    # post = relationship("Post", back_populates="comments")
+
+    def __repr__(self):
+        return f"{self.__dict__}"
+
+
+Base.metadata.create_all(engine)
